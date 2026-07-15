3b1a9ff530bbf7c6c83cacdf35286d161a574423
Dinesh6992 <pdineshmani005@gmail.com>
2026-07-15 05:02:29 +0000

    prg

--- practical3.java
+++ practical3.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
  class practical3 {
-	public static void main(String[] args)
+	public static void main(String args[])
 	{
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter a line text:");
